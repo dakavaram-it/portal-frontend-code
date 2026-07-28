2dc01c3aaa12716c3573e65a4e4b4d87d365f66d
Nithish <nithish@mytdp.com>
2026-07-28 19:47:52 +0530

    Updated vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
   preview: {
     port: 9001,
     host: '0.0.0.0',
+    allowedHosts: ['portalnew.mypartydashboard.com'],
     proxy: { '/api': { target: 'http://127.0.0.1:8001', rewrite: (p) => p.replace(/^\/api/, '') } },
   },
 })
